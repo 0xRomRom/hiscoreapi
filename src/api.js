fbaab7b954cd00eff4b361f8d4eab803056c07db
Roman - <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>
2023-10-19 16:53:21 +0200

    Working

---
+++ src/api.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios").default;
+
+const app = express();
+
+const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+router.post("/", async (req, res) => {
+  const user = req.body.user;
+  console.log(user);
+  const URL = `https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${user}`;
+  try {
+    const response = await axios({
+      url: URL,
+      method: "GET",
+    });
+
+    res.json({ result: response.data });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching the data" });
+  }
+});
+
+app.use("/.netlify/functions/api", router);
+
+module.exports.handler = serverless(app);
